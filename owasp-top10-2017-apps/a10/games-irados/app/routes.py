8e82fbd3a4931055824f913320e71cea173b132f
Diego Chavão <diego.chavao@corp.globo.com>
2019-01-10 11:25:02 -0200

    [A10] Mount app folder as docker volume.

--- owasp-top10-2017-apps/a10/games-irados/app/routes.py
+++ owasp-top10-2017-apps/a10/games-irados/app/routes.py
@@ -137,4 +137,4 @@ if __name__ == '__main__':
     dbName = os.environ.get('MYSQL_DB')
     database = DataBase(dbEndpoint, dbUser, dbPassword, dbName)
     init_db(database)
-    app.run(host='0.0.0.0',port=3001, debug=False)
+    app.run(host='0.0.0.0',port=3001, debug=True)
