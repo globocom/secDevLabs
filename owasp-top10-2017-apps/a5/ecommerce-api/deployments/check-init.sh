ccea94eef04aa303d83cbfe9fa81e303d38ac19f
Krlier <daniel.carlier@hotmail.com>
2019-02-05 23:26:11 -0200

    [REFACT] Make A5 initialization message check if app has started

---
+++ owasp-top10-2017-apps/a5/ecommerce-api/deployments/check-init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# This script prints initialization messages after the compose phase.
+#
+
+COLOR_RESET='\033[0m'
+COLOR_YELLOW='\033[33m'
+COLOR_GREEN='\033[32m'
+COLOR_BLUE='\033[1;34m'
+
+PROJECT='A5 Vulnerable Ecommerce API'
+PORT=8888
+TRIES=20
+
+printf "${COLOR_YELLOW}SecDevLabs: 👀  Your app is starting!\n${COLOR_RESET}"
+while : ;do
+    `curl -s -f http://localhost:$PORT > /dev/null`
+    if [ $? == 0 ]; then
+        break
+    fi
+    if [ $TRIES == 20 ]; then
+        printf "${COLOR_GREEN}SecDevLabs: ☕️  Hmmm... Sounds good!${COLOR_RESET}\n"
+    fi
+    if [ $TRIES == 0 ]; then
+        break
+    fi
+    sleep 5
+    TRIES=$TRIES-1
+    printf "${COLOR_YELLOW}SecDevLabs: 👀  Your app is still starting...\n${COLOR_RESET}"
+done
+
+if [ $TRIES == 0 ]; then
+    printf "${COLOR_BLUE}SecDevLabs: Ooops! Something went wrong, please check api details for more information!"
+else
+    printf "${COLOR_GREEN}SecDevLabs: 🔥  ${PROJECT} is now running at ${COLOR_RESET}${COLOR_BLUE}http://localhost:$PORT${COLOR_RESET}\n"
+fi
