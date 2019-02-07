9b7a061cec45f024fad8db4b944242318792aa8c
Pedro Kiefer <pedro.kiefer@corp.globo.com>
2019-02-07 16:14:00 -0200

    [A8][mitigation] Use pyjwt instead of pickle

--- owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
+++ owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
@@ -2,8 +2,8 @@
 
 from flask import Flask, request, make_response, render_template, redirect, flash
 import uuid
-import pickle
-import base64
+import jwt
+import os
 app = Flask(__name__)
 
 
@@ -11,19 +11,26 @@ app = Flask(__name__)
 def ola():
     return render_template('index.html')
 
-@app.route("/admin", methods=['GET','POST'])
+
+@app.route("/admin", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.values.get('username')
         password = request.values.get('password')
-    
+
         if username == "admin" and password == "admin":
             token = str(uuid.uuid4().hex)
-            cookie = { "username":username, "admin":True, "sessionId":token }
-            pickle_resultado = pickle.dumps(cookie)
-            encodedSessionCookie = base64.b64encode(pickle_resultado)
+            cookie_data = {
+                "username": username,
+                "admin": True,
+                "sessionId": token
+            }
+
+            cookie = jwt.encode(cookie_data,
+                                os.environ.get('COOKIE_SECRET'),
+                                algorithm='HS256')
             resp = make_response(redirect("/user"))
-            resp.set_cookie("sessionId", encodedSessionCookie)
+            resp.set_cookie("sessionId", cookie)
             return resp
 
         else:
@@ -32,17 +39,19 @@ def login():
     else:
         return render_template('admin.html')
 
+
 @app.route("/user", methods=['GET'])
 def userInfo():
     cookie = request.cookies.get("sessionId")
-    if cookie == None:
+    if cookie is None:
         return "Não Autorizado!"
-    cookie = pickle.loads(base64.b64decode(cookie))
+
+    cookie = jwt.decode(cookie,
+                        os.environ.get('COOKIE_SECRET'),
+                        algorithms=['HS256'])
 
     return render_template('user.html')
-    
-
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
