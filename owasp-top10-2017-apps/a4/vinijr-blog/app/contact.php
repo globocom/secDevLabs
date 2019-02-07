87a103893cf22cb253013374fab1685c49eb8abd
Pedro Kiefer <pedro.kiefer@corp.globo.com>
2019-02-07 17:15:32 -0200

    [A4][MITIGATION] Disable xml entity loader

--- owasp-top10-2017-apps/a4/vinijr-blog/app/contact.php
+++ owasp-top10-2017-apps/a4/vinijr-blog/app/contact.php
@@ -1,7 +1,9 @@
 <?php
+libxml_disable_entity_loader(true);
+
 $xmlfile = file_get_contents('php://input');
 $dom = new DOMDocument();
-$dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD);
+$dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD | LIBXML_DTDATTR | LIBXML_NONET);
 $contact = simplexml_import_dom($dom);
 $name = $contact->name;
 $email = $contact->email;
