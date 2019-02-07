6f4004cae3088957a70a4cb4575e8fb84f703883
Vitória Rio <vitoria.mdrio@gmail.com>
2019-02-07 11:41:34 -0200

    [FIX] Change json fields name

--- owasp-top10-2017-apps/a3/snake-pro/app/types/types.go
+++ owasp-top10-2017-apps/a3/snake-pro/app/types/types.go
@@ -2,14 +2,14 @@ package types
 
 // LoginAttempt is the struct that stores all data from a login attempt.
 type LoginAttempt struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"user"`
+	Password string `json:"pass"`
 }
 
 // UserData is the strucht that holds all data from user from MongoDB.
 type UserData struct {
-	Username       string `json:"username" bson:"username"`
-	Password       string `json:"password" bson:"password"`
-	RepeatPassword string `json:"repeat-password"`
+	Username       string `json:"user" bson:"username"`
+	Password       string `json:"pass" bson:"password"`
+	RepeatPassword string `json:"passcheck"`
 	UserID         string `bson:"userID"`
 }
