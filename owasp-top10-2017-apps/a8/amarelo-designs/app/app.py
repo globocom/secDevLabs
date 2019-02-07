5122bd3f4625868bb362433dab8ade7b3d7a7161
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2019-02-07 16:34:26 -0200

    fix: unserialize-secure-issue

--- owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
+++ owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
@@ -1,11 +1,16 @@
 # coding: utf-8
 
-from flask import Flask, request, make_response, render_template, redirect, flash
 import uuid
-import pickle
 import base64
-app = Flask(__name__)
 
+import jwt
+
+from flask import Flask, request, make_response, render_template, redirect, flash
+
+
+app = Flask(__name__)
+vulnerable_secret = 'app-vulnerable-secret'
+sessions_db = {}
 
 @app.route("/")
 def ola():
@@ -16,14 +21,20 @@ def login():
     if request.method == 'POST':
         username = request.values.get('username')
         password = request.values.get('password')
-    
+
         if username == "admin" and password == "admin":
-            token = str(uuid.uuid4().hex)
-            cookie = { "username":username, "admin":True, "sessionId":token }
-            pickle_resultado = pickle.dumps(cookie)
-            encodedSessionCookie = base64.b64encode(pickle_resultado)
+            session_id = str(uuid.uuid4().hex)
+            cookie = {
+                "username": username,
+                "admin": True,
+                "sessionId": session_id,
+            }
+
+            encodedSessionCookie = jwt.encode(cookie, vulnerable_secret, algorithm='HS256')
             resp = make_response(redirect("/user"))
             resp.set_cookie("sessionId", encodedSessionCookie)
+            sessions_db[session_id] = cookie
+
             return resp
 
         else:
@@ -34,14 +45,26 @@ def login():
 
 @app.route("/user", methods=['GET'])
 def userInfo():
-    cookie = request.cookies.get("sessionId")
-    if cookie == None:
+    raw_cookie = request.cookies.get("sessionId")
+    if raw_cookie == None:
         return "Não Autorizado!"
-    cookie = pickle.loads(base64.b64decode(cookie))
+
+    cookie = jwt.decode(raw_cookie, vulnerable_secret, algorithms=['HS256'])
+    session = sessions_db.get(cookie.get('sessionId'))
+    if not session:
+        return "Não Autorizado!"
+
+    username_session = session.get('username')
+    username_cookie = cookie.get('username')
+    if username_cookie != username_session:
+        return "Não Autorizado!"
+
+    admin_session = session.get('admin')
+    admin_cookie = cookie.get('admin')
+    if admin_cookie != admin_session:
+        return "Não Autorizado!"
 
     return render_template('user.html')
-    
-
 
 
 if __name__ == '__main__':
