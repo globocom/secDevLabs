ceb2d0e263b8e6e301ad4f327f3f397032300c57
Danilo Gila de Santana <danilosantana58@gmail.com>
2019-02-07 12:17:29 -0200

    Add pbkdf2 cryptography function to reduce vulnerability

--- owasp-top10-2017-apps/a3/insecure-go-project/deployments/mongo-init.js
+++ owasp-top10-2017-apps/a3/insecure-go-project/deployments/mongo-init.js
@@ -1,9 +1,11 @@
 var db = connect("mongodb://localhost/insecure_go_project");
+var pbkdf2 = require('pbkdf2');
+var protectedKey = pbkdf2.pbkdf2Sync('secret', 'salt', 1000, 32, 'sha512')
 
 db.createUser(
     {
         user: "u_insecure_go_project",
-        pwd: "svGX8SViufvYYNu6m3Kv",
+        pwd: protectedKey,
         roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
     }
 );
