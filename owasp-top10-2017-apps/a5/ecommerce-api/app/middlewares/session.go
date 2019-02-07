54d8f75d30206dcc7753b6bb4e778813ae6355f9
Igor Cescon de Moura <igor.moura@corp.globo.com>
2019-02-07 18:39:28 -0200

    fix: return a generic message for the user when failing authentication

--- owasp-top10-2017-apps/a5/ecommerce-api/app/middlewares/session.go
+++ owasp-top10-2017-apps/a5/ecommerce-api/app/middlewares/session.go
@@ -14,7 +14,7 @@ func RequireLogin(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		cookie, err := c.Cookie("sessionIDa5")
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": err.Error()})
+			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Invalid user cookie."})
 		}
 
 		token, err := jwt.Parse(cookie.Value, func(token *jwt.Token) (interface{}, error) {
@@ -24,7 +24,7 @@ func RequireLogin(next echo.HandlerFunc) echo.HandlerFunc {
 			return []byte(os.Getenv("A5-JWT-SECRET")), nil
 		})
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": err.Error()})
+			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Invalid user cookie."})
 		}
 		if !token.Valid {
 			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Invalid user cookie."})
@@ -32,12 +32,12 @@ func RequireLogin(next echo.HandlerFunc) echo.HandlerFunc {
 
 		claims, ok := token.Claims.(jwt.MapClaims)
 		if !ok {
-			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Could not retrieve jwt claims from cookie."})
+			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Invalid user cookie."})
 		}
 
 		username, ok := claims["name"]
 		if !ok {
-			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Could not retrieve username from cookie."})
+			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Invalid user cookie."})
 		}
 		c.Set("username", username)
 		return next(c)
