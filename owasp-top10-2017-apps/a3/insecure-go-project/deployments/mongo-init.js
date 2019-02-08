0a64729d6c5cf7dfc97a65f0621746ff7e49150c
Danilo Gila de Santana <danilosantana58@gmail.com>
2019-02-08 10:58:21 -0200

    Remove sensitive files with credencials

--- owasp-top10-2017-apps/a3/insecure-go-project/deployments/mongo-init.js
+++
@@ -1,8 +0,0 @@
-var db = connect("mongodb://localhost/insecure_go_project");
-db.createUser(
-    {
-        user: "huskyUser2613311233",
-        pwd: "huskyPass31979227",
-        roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
-    }
-);
