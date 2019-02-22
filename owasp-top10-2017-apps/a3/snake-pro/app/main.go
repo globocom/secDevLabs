419ac97ecb73c0530b29254932d72c423e1e9bc8
Gabriel903 <gabriel.valensuela@gmail.com>
2019-02-22 17:26:01 -0300

    Add TLS certificate

--- owasp-top10-2017-apps/a3/snake-pro/app/main.go
+++ owasp-top10-2017-apps/a3/snake-pro/app/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"net/http"
 	"os"
 	"strconv"
 
@@ -58,6 +59,21 @@ func main() {
 
 	echoInstance := echo.New()
 	echoInstance.HideBanner = true
+
+	echoInstance.GET("/request", func(c echo.Context) error {
+		req := c.Request()
+		format := `
+		  <code>
+			Protocol: %s<br>
+			Host: %s<br>
+			Remote Address: %s<br>
+			Method: %s<br>
+			Path: %s<br>
+		  </code>
+		`
+		return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, req.RemoteAddr, req.Method, req.URL.Path))
+	})
+	echoInstance.StartTLS(":10033", "cert.pem", "key.pem")
 
 	echoInstance.Use(middleware.Logger())
 	echoInstance.Use(middleware.Recover())
