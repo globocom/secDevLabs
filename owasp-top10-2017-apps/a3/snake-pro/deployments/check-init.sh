9663f6aee8759a10a33e15cc195e0471d4e49d9b
Cristian <cristian.dean@corp.globo.com>
2019-02-22 16:45:26 -0300

    Add http support

--- owasp-top10-2017-apps/a3/snake-pro/deployments/check-init.sh
+++ owasp-top10-2017-apps/a3/snake-pro/deployments/check-init.sh
@@ -10,14 +10,13 @@ COLOR_BLUE='\033[1;34m'
 COLOR_RESET='\033[0m'
 
 PROJECT='A3 - Snake-Pro'
-PORT=10033
 TRIES=480
 LOADING=0
 
 printf "${COLOR_YELLOW}SecDevLabs: 👀  Your app is starting!\n${COLOR_RESET}"
 
 while : ; do
-    `curl -s -f http://localhost:$PORT/login > /dev/null`
+    `curl -s -f --insecure https://localhost/login > /dev/null`
     if [ $? == 0 ] ; then
         break
     fi
@@ -68,5 +67,5 @@ done
 if [ $TRIES == 0 ]; then
     printf "\n${COLOR_RED}SecDevLabs: Ooops! Something went wrong, please check api details for more information!\n${COLOR_RESET}"
 else
-    printf "\n${COLOR_GREEN}SecDevLabs: 🔥  ${PROJECT} is now running at ${COLOR_RESET}${COLOR_BLUE}http://localhost:$PORT${COLOR_RESET}\n"
+    printf "\n${COLOR_GREEN}SecDevLabs: 🔥  ${PROJECT} is now running at ${COLOR_RESET}${COLOR_BLUE}https://localhost\n"
 fi
