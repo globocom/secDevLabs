32bc5959125f45bb402142de95d0a76defa46e04
Gabriel903 <gabriel.valensuela@gmail.com>
2019-02-22 11:56:24 -0300

    Fix problem with handlers and change the messages

--- owasp-top10-2017-apps/a5/ecommerce-api/app/handlers/handlers.go
+++ owasp-top10-2017-apps/a5/ecommerce-api/app/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/globocom/secDevLabs/owasp-top10-2017-apps/a5/ecommerce-api/app/db"
@@ -20,33 +21,28 @@ func GetTicket(c echo.Context) error {
 	userDataQuery := map[string]interface{}{"userID": id}
 	cookie, err := c.Cookie("sessionIDa5")
 	if err != nil {
-		return fmt.Errorf("Error with cookie", err)
+		return c.String(http.StatusBadRequest, "Cookie missing")
 	}
 	token, err := jwt.Parse(cookie.Value, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return []byte("secret"), nil
+		return []byte(os.Getenv("SECRET_KEY")), nil
 	})
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Invalid cookie")
+	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		userDataResult, err := db.GetUserData(userDataQuery)
 		if err != nil {
 			// could not find this user in MongoDB (or MongoDB err connection)
-			return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error finding this UserID."})
+			return c.JSON(http.StatusForbidden, map[string]string{"result": "error", "details": "Request failed."})
 
 		}
 		if claims["name"] != userDataResult.Username {
-			return c.JSON(http.StatusForbidden, map[string]string{"result": "error", "details": "I wont give you any error messages so you dont have hints to access my application"})
-		}
-		format := c.QueryParam("format")
-		if format == "json" {
-			return c.JSON(http.StatusOK, map[string]string{
-				"result":   "success",
-				"username": userDataResult.Username,
-				"ticket":   userDataResult.Ticket,
-			})
+			return c.JSON(http.StatusForbidden, map[string]string{"result": "error", "details": "Request failed."})
 		}
 		msgTicket := fmt.Sprintf("Hey, %s! This is your ticket: %s\n", userDataResult.Username, userDataResult.Ticket)
 		return c.String(http.StatusOK, msgTicket)
