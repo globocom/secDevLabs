80a3102c0741292098662d7e8c1f982f40477b5e
Cristian Dean <cristiandeanabreu@gmail.com>
2019-02-24 17:38:36 -0300

    Fix unsafe cookie use

--- owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
+++ owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
@@ -1,10 +1,16 @@
 # coding: utf-8
 
-from flask import Flask, request, make_response, render_template, redirect, flash
+from flask import Flask, request, make_response, render_template, redirect, flash, session
 import uuid
 import pickle
 import base64
+import os
 app = Flask(__name__)
+
+app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
+
+if app.secret_key == 'default_secret_key':
+    print("[UNSAFE MODE] - The SECRET_KEY has not been set!")
 
 
 @app.route("/")
@@ -16,33 +22,19 @@ def login():
     if request.method == 'POST':
         username = request.values.get('username')
         password = request.values.get('password')
-    
         if username == "admin" and password == "admin":
             token = str(uuid.uuid4().hex)
-            cookie = { "username":username, "admin":True, "sessionId":token }
-            pickle_resultado = pickle.dumps(cookie)
-            encodedSessionCookie = base64.b64encode(pickle_resultado)
-            resp = make_response(redirect("/user"))
-            resp.set_cookie("sessionId", encodedSessionCookie)
-            return resp
-
-        else:
-            return redirect("/admin")
-
-    else:
-        return render_template('admin.html')
+            session['userSession'] = { "username":username, "admin":True, "sessionId":token }
+            return redirect("/user")
+        return redirect("/admin")
+    return render_template('admin.html')
 
 @app.route("/user", methods=['GET'])
 def userInfo():
     cookie = request.cookies.get("sessionId")
-    if cookie == None:
+    if 'userSession' not in session:
         return "Não Autorizado!"
-    cookie = pickle.loads(base64.b64decode(cookie))
-
     return render_template('user.html')
     
-
-
-
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
