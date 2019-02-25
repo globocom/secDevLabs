ff883d888f0d4692e9b470ab49a4ebdcca5ce38b
Cristian <cristian.dean@corp.globo.com>
2019-02-25 19:02:45 -0300

    Add jwt support

--- owasp-top10-2017-apps/a5/ecommerce-api/app/handlers/handlers.go
+++ owasp-top10-2017-apps/a5/ecommerce-api/app/handlers/handlers.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/globocom/secDevLabs/owasp-top10-2017-apps/a5/ecommerce-api/app/db"
 	"github.com/labstack/echo"
 )
@@ -13,14 +15,44 @@ func HealthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "WORKING\n")
 }
 
+func extractClaims(tokenStr string) (jwt.MapClaims, bool) {
+	hmacSecretString := "secret" // Value
+	hmacSecret := []byte(hmacSecretString)
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return hmacSecret, nil
+	})
+
+	if err != nil {
+		return nil, false
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, true
+	} else {
+		log.Printf("Invalid JWT Token")
+		return nil, false
+	}
+}
+
 // GetTicket returns the userID ticket.
 func GetTicket(c echo.Context) error {
-	id := c.Param("id")
+
+	cookie, err := c.Cookie("sessionIDa5")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error finding tickets."})
+	}
+
+	claims, ok := extractClaims(cookie.Value)
+	if !ok {
+		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error finding tickets."})
+	}
+
+	id := claims["id"]
 	userDataQuery := map[string]interface{}{"userID": id}
 	userDataResult, err := db.GetUserData(userDataQuery)
 	if err != nil {
 		// could not find this user in MongoDB (or MongoDB err connection)
-		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error finding this UserID."})
+		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error finding tickets."})
 	}
 
 	format := c.QueryParam("format")
