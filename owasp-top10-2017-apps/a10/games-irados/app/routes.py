60a9208302cda6421d496865d5a3ec9312ff4df8
Cristian <cristian.dean@corp.globo.com>
2019-02-25 14:12:44 -0300

    Add captcha to /login and /coupon

--- owasp-top10-2017-apps/a10/games-irados/app/routes.py
+++ owasp-top10-2017-apps/a10/games-irados/app/routes.py
@@ -3,6 +3,8 @@
 from functools import wraps
 import uuid
 import datetime
+import json
+import requests
 from flask import (
     Flask,
     render_template,
@@ -28,6 +30,9 @@ bootstrap = Bootstrap(app)
 
 app.config.from_pyfile('config.py')
 
+RECAPTCHA_SITE_KEY = os.getenv('RECAPTCHA_SITE_KEY', "")
+RECAPTCHA_SITE_SECRET = os.getenv('RECAPTCHA_SITE_SECRET', "")
+
 
 def generate_csrf_token():
     '''
@@ -40,6 +45,7 @@ def generate_csrf_token():
 
 app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
+
 @app.before_request
 def csrf_protect():
     '''
@@ -51,6 +57,7 @@ def csrf_protect():
         if not token_csrf or str(token_csrf) != str(form_token):
             return "ERROR: Wrong value for csrf_token"
 
+
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -60,9 +67,11 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+
 @app.route('/', methods=['GET'])
 def root():
     return redirect('/login')
+
 
 @app.route('/logout', methods=['GET'])
 @login_required
@@ -70,19 +79,24 @@ def logout():
     session.clear()
     return redirect('/login')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        if not is_human(request):
+            flash("Captcha inválido!.", "danger")
+            return render_template('login.html', sitekey=RECAPTCHA_SITE_KEY)
         username = request.form.get('username').encode('utf-8')
         psw = Password(request.form.get('password').encode('utf-8'))
         user_password, success = database.get_user_password(username)
         if not success or user_password == None or not psw.validate_password(str(user_password[0])):
             flash("Usuario ou senha incorretos", "danger")
-            return render_template('login.html')
+            return render_template('login.html', sitekey=RECAPTCHA_SITE_KEY)
         session['username'] = username
         return redirect('/home')
     else:
-        return render_template('login.html')
+        return render_template('login.html', sitekey=RECAPTCHA_SITE_KEY)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def newuser():
@@ -107,28 +121,44 @@ def newuser():
     else:
         return render_template('register.html')
 
+
 @app.route('/home', methods=['GET'])
 @login_required
 def home():
     return render_template('index.html')
 
+
 @app.route('/coupon', methods=['GET', 'POST'])
 @login_required
 def cupom():
     if request.method == 'POST':
+        if not is_human(request):
+            flash("Captcha inválido!.", "danger")
+            return render_template('coupon.html', sitekey=RECAPTCHA_SITE_KEY)
         coupon = request.form.get('coupon')
-        rows, success = database.get_game_coupon(coupon, session.get('username'))
+        rows, success = database.get_game_coupon(
+            coupon, session.get('username'))
         if not success or rows == None or rows == 0:
             flash("Cupom invalido", "danger")
-            return render_template('coupon.html')
+            return render_template('coupon.html', sitekey=RECAPTCHA_SITE_KEY)
         game, success = database.get_game(coupon, session.get('username'))
         if not success or game == None:
             flash("Cupom invalido", "danger")
-            return render_template('coupon.html')
+            return render_template('coupon.html', sitekey=RECAPTCHA_SITE_KEY)
         flash("Voce ganhou {}".format(game[0]), "primary")
-        return render_template('coupon.html')
-    else:
-        return render_template('coupon.html')
+    return render_template('coupon.html', sitekey=RECAPTCHA_SITE_KEY)
+
+
+def is_human(request):
+    if 'g-recaptcha-response' not in request.form:
+        return False
+    captcha_response = request.form['g-recaptcha-response']
+    payload = {'response': captcha_response, 'secret': RECAPTCHA_SITE_SECRET}
+    response = requests.post(
+        "https://www.google.com/recaptcha/api/siteverify", payload)
+    response_text = json.loads(response.text)
+    return response_text['success']
+
 
 if __name__ == '__main__':
     dbEndpoint = os.environ.get('MYSQL_ENDPOINT')
@@ -137,4 +167,4 @@ if __name__ == '__main__':
     dbName = os.environ.get('MYSQL_DB')
     database = DataBase(dbEndpoint, dbUser, dbPassword, dbName)
     init_db(database)
-    app.run(host='0.0.0.0',port=3001, debug=True)
+    app.run(host='0.0.0.0', port=3001, debug=True)
