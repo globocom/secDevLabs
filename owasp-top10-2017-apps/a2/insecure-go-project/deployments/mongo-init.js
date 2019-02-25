75173d96b8802fe335b19490cc892005372c3254
Gabriel903 <gabriel.valensuela@gmail.com>
2019-02-25 13:44:58 -0300

    Fix protection of env variables and adding a generator passwords

--- owasp-top10-2017-apps/a2/insecure-go-project/deployments/mongo-init.js
+++ owasp-top10-2017-apps/a2/insecure-go-project/deployments/mongo-init.js
@@ -1,9 +1,8 @@
 var db = connect("mongodb://localhost/insecure_go_project");
-
 db.createUser(
     {
-        user: "u_insecure_go_project",
-        pwd: "svGX8SViufvYYNu6m3Kv",
+        user: "User828822221",
+        pwd: "Pass2085519227",
         roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
     }
 );
