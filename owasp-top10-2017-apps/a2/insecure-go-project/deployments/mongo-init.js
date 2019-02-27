e0db34d6b7be2b7972decc68e034dc2c7450b987
Gabriel903 <gabriel.valensuela@gmail.com>
2019-02-27 13:48:54 -0300

    Fixed

--- owasp-top10-2017-apps/a2/insecure-go-project/deployments/mongo-init.js
+++
@@ -1,8 +0,0 @@
-var db = connect("mongodb://localhost/insecure_go_project");
-db.createUser(
-    {
-        user: "User828822221",
-        pwd: "Pass2085519227",
-        roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
-    }
-);
