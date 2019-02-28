fd0bb3468aa733c3fd4a5ee9410032fa28576410
Michael Benford <michael.benford@corp.globo.com>
2019-02-28 19:56:49 -0300

    Replace pickle with json as a cookie serializer

--- owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
+++ owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, make_response, render_template, redirect, flash
 import uuid
-import pickle
+import json
 import base64
 app = Flask(__name__)
 
@@ -20,8 +20,8 @@ def login():
         if username == "admin" and password == "admin":
             token = str(uuid.uuid4().hex)
             cookie = { "username":username, "admin":True, "sessionId":token }
-            pickle_resultado = pickle.dumps(cookie)
-            encodedSessionCookie = base64.b64encode(pickle_resultado)
+            json_resultado = json.dumps(cookie)
+            encodedSessionCookie = base64.b64encode(json_resultado.encode('utf-8'))
             resp = make_response(redirect("/user"))
             resp.set_cookie("sessionId", encodedSessionCookie)
             return resp
@@ -37,7 +37,7 @@ def userInfo():
     cookie = request.cookies.get("sessionId")
     if cookie == None:
         return "Não Autorizado!"
-    cookie = pickle.loads(base64.b64decode(cookie))
+    cookie = json.loads(base64.b64decode(cookie.encode('utf-8')))
 
     return render_template('user.html')
     
