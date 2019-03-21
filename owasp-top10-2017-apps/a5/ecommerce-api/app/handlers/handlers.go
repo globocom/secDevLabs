cdfecbb012de60e764b278bc0149cc4ac51f3d72
Lourenço Sander <lousander13@gmail.com>
2019-03-21 11:56:15 -0300

    Update GetTicket function to validate the user identity

--- owasp-top10-2017-apps/a5/ecommerce-api/app/handlers/handlers.go
+++ owasp-top10-2017-apps/a5/ecommerce-api/app/handlers/handlers.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"os"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/globocom/secDevLabs/owasp-top10-2017-apps/a5/ecommerce-api/app/db"
 	"github.com/labstack/echo"
 )
@@ -15,23 +17,38 @@ func HealthCheck(c echo.Context) error {
 
 // GetTicket returns the userID ticket.
 func GetTicket(c echo.Context) error {
-	id := c.Param("id")
-	userDataQuery := map[string]interface{}{"userID": id}
+	cookie, err := ReadCookie(c)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error fetching tickets."})
+	}
+
+	token, _ := jwt.Parse(cookie, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method")
+		}
+		return []byte(os.Getenv("A5_SECRET")), nil
+	})
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return c.JSON(http.StatusForbidden, map[string]string{"result": "error", "details": "Error fetching tickets."})
+	}
+
+	userID := claims["userid"]
+	userDataQuery := map[string]interface{}{"userID": userID}
 	userDataResult, err := db.GetUserData(userDataQuery)
 	if err != nil {
 		// could not find this user in MongoDB (or MongoDB err connection)
 		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error finding this UserID."})
 	}
-
-	format := c.QueryParam("format")
-	if format == "json" {
-		return c.JSON(http.StatusOK, map[string]string{
-			"result":   "success",
-			"username": userDataResult.Username,
-			"ticket":   userDataResult.Ticket,
-		})
+	if claims["username"] != userDataResult.Username {
+		return c.JSON(http.StatusForbidden, map[string]string{"result": "error", "details": "Error fetching tickets."})
 	}
-
+	return c.JSON(http.StatusOK, map[string]string{
+		"result":   "success",
+		"username": userDataResult.Username,
+		"ticket":   userDataResult.Ticket,
+	})
 	msgTicket := fmt.Sprintf("Hey, %s! This is your ticket: %s\n", userDataResult.Username, userDataResult.Ticket)
 	return c.String(http.StatusOK, msgTicket)
 }
