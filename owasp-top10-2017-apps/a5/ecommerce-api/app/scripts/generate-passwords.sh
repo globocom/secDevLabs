d540e6c84bd7976a460b419781e8941e67f49dd1
Lourenço Sander <lousander13@gmail.com>
2019-03-21 11:52:59 -0300

    Add secret to jwt as environment variable created randomly

--- owasp-top10-2017-apps/a5/ecommerce-api/app/scripts/generate-passwords.sh
+++ owasp-top10-2017-apps/a5/ecommerce-api/app/scripts/generate-passwords.sh
@@ -4,16 +4,19 @@
 #
 
 # Generating "random" passwords
+SECRET_TMP="$RANDOM$RANDOM$RANDOM"
 MONGO_DATABASE_USERNAME_TMP="User$RANDOM$RANDOM"
 MONGO_DATABASE_PASSWORD_TMP="Pass$RANDOM$RANDOM"
 
 # Writing passwords into dockers.env file to be used by docker compose
 echo "MONGO_DATABASE_USERNAME=$MONGO_DATABASE_USERNAME_TMP" > deployments/dockers.env
 echo "MONGO_DATABASE_PASSWORD=$MONGO_DATABASE_PASSWORD_TMP" >> deployments/dockers.env
+echo "A5_SECRET"=$SECRET_TMP >> deployments/dockers.env
 
 # Writing passwords into .env to be used by run_create_cert.sh and to send to STDOUT
 echo "export MONGO_DATABASE_USERNAME=\"$MONGO_DATABASE_USERNAME_TMP\"" > .env
 echo "export MONGO_DATABASE_PASSWORD=\"$MONGO_DATABASE_PASSWORD_TMP\"" >> .env
+echo "export A5_SECRET=\"SECRET_TMP\"" >> .env
 
 # Preparing script to create mongoDB default user
 cat << EOF > deployments/mongo-init.js
