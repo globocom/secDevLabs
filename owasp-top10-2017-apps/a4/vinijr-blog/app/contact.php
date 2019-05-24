248eac4176ac98f1dfffd3771bb10177cc8cc249
Italo Valcy <italo@pop-ba.rnp.br>
2019-05-24 10:07:05 -0300

    fix error of using insecure flags of XML parser

--- owasp-top10-2017-apps/a4/vinijr-blog/app/contact.php
+++ owasp-top10-2017-apps/a4/vinijr-blog/app/contact.php
@@ -1,7 +1,8 @@
 <?php
 $xmlfile = file_get_contents('php://input');
 $dom = new DOMDocument();
-$dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD);
+//$dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD);
+$dom->loadXML($xmlfile);
 $contact = simplexml_import_dom($dom);
 $name = $contact->name;
 $email = $contact->email;
