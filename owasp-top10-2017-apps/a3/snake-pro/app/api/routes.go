56435777d8a99bddecbb6cb9ba5783c8ffd420cd
Tiago Silva Leite <42103618+tsleite@users.noreply.github.com>
2019-10-16 09:10:16 -0300

    Add library bcrypt

--- owasp-top10-2017-apps/a3/snake-pro/app/api/routes.go
+++ owasp-top10-2017-apps/a3/snake-pro/app/api/routes.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 	"os"
 	"time"
-
+	
 	jwt "github.com/dgrijalva/jwt-go"
 	db "github.com/globocom/secDevLabs/owasp-top10-2017-apps/a3/snake-pro/app/db/mongo"
 	"github.com/globocom/secDevLabs/owasp-top10-2017-apps/a3/snake-pro/app/pass"
 	"github.com/globocom/secDevLabs/owasp-top10-2017-apps/a3/snake-pro/app/types"
 	"github.com/google/uuid"
 	"github.com/labstack/echo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // HealthCheck is the heath check function.
