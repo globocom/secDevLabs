bdda7a411a7ad8b5ab6090415339f5bd936ddefc
Daniel Carlier[user] <daniel.carlier@hotmail.com>
2020-04-22 15:41:27 -0300

    gitignore|FEAT: Update gitignore

--- owasp-top10-2016-mobile/m2/cool_games/server/deployments/mongo-init.js
+++
@@ -1,8 +0,0 @@
-var db = connect("mongodb://localhost/coolgames");
-db.createUser(
-    {
-        user: "User1964110909",
-        pwd: "Pass1658131492",
-        roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
-    }
-);
