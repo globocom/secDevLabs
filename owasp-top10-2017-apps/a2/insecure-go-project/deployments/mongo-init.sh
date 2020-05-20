5d318b3eac32b267c1b283b7846a90ffcb9d2a5c
Se Wan de Campos <se.wan@g.globo>
2020-05-20 17:44:04 -0300

    Remove the + character at the beginning of lines

--- owasp-top10-2017-apps/a2/insecure-go-project/deployments/mongo-init.sh
+++ owasp-top10-2017-apps/a2/insecure-go-project/deployments/mongo-init.sh
@@ -1,11 +1,11 @@
-+mongo << EOF
-+var db = connect("mongodb://localhost/insecure_go_project");
-+
-+db.createUser(
-+    {
-+        user: "$MONGO_USER",
-+        pwd: "$MONGO_PASSWORD",
-+        roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
-+    }
-+);
-+EOF
+mongo << EOF
+var db = connect("mongodb://localhost/insecure_go_project");
+
+db.createUser(
+    {
+        user: "$MONGO_USER",
+        pwd: "$MONGO_PASSWORD",
+        roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
+    }
+);
+EOF
