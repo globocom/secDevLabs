83f4abe76fc7b8bfce6481cd55b4e98f7d971dcc
Se Wan de Campos <se.wan@g.globo>
2020-06-02 11:45:54 -0300

    Add the FLAG_SECURE to disable the auto-generated screenshot

--- owasp-top10-2016-mobile/m2/cool_games/mobile/android/app/src/main/kotlin/com/secdevlabs/cool/games/MainActivity.kt
+++ owasp-top10-2016-mobile/m2/cool_games/mobile/android/app/src/main/kotlin/com/secdevlabs/cool/games/MainActivity.kt
@@ -4,9 +4,11 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.view.WindowManager.LayoutParams;
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        getWindow().addFlags(LayoutParams.FLAG_SECURE);
     }
 }
