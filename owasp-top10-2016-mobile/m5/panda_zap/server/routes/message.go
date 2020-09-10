a13c51d745c9c0cf0e9fa3ba9e86bbdc5b9ba188
Daniel Carlier <daniel.carlier@hotmail.com>
2020-09-10 14:16:44 -0300

    server|FEAT: Add V2 logic to the message pack

--- owasp-top10-2016-mobile/m5/panda_zap/server/routes/message.go
+++ owasp-top10-2016-mobile/m5/panda_zap/server/routes/message.go
@@ -4,23 +4,34 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/globocom/secDevLabs/owasp-top10-2016-mobile/m5/panda_zap/server/message"
 	"github.com/labstack/echo"
 )
 
 // GetUserMessages returns all pending user messages.
 func (es *EchoServer) GetUserMessages(c echo.Context) error {
+
 	username := es.Auth.GetUser(c)
 
-	userFromDB, err := es.Database.GetUserWithoutKeyWipe(username)
+	userFromDB, err := es.Database.GetUser(username)
 	if err != nil {
 		es.Logger.Error("Error getting user from database: ", err)
 		return c.JSON(http.StatusInternalServerError,
 			map[string]string{"result": "fail", "message": err.Error()})
 	}
 
+	messagesToBeSent := userFromDB.Messages
+	userFromDB.Messages = []message.Message{}
+
+	if err := es.Database.InsertUser(userFromDB); err != nil {
+		es.Logger.Error("Error inserting user in the database: ", err)
+		return c.JSON(http.StatusInternalServerError,
+			map[string]string{"result": "fail", "message": err.Error()})
+	}
+
 	es.Logger.Info(fmt.Sprintf("Successfully got messages for user: '%s'", username))
 
-	return c.JSON(http.StatusOK, userFromDB)
+	return c.JSON(http.StatusOK, messagesToBeSent)
 }
 
 // UpdateMessages with ones yet to be delivered.
@@ -34,9 +45,17 @@ func (es *EchoServer) UpdateMessages(c echo.Context) error {
 			map[string]string{"result": "fail", "message": err.Error()})
 	}
 
-	if err := es.Database.UpdateUserMessages(incomingUser.Name, incomingUser.Messages); err != nil {
-		es.Logger.Warn("Error updating user messages", err)
+	userFromDB, err := es.Database.GetUser(incomingUser.Name)
+	if err != nil {
+		es.Logger.Error("Error getting user from database: ", err)
+		return c.JSON(http.StatusInternalServerError,
+			map[string]string{"result": "fail", "message": err.Error()})
+	}
 
+	userFromDB.Messages = append(userFromDB.Messages, incomingUser.Messages...)
+
+	if err := es.Database.InsertUser(userFromDB); err != nil {
+		es.Logger.Error("Error getting user from database: ", err)
 		return c.JSON(http.StatusInternalServerError,
 			map[string]string{"result": "fail", "message": err.Error()})
 	}
