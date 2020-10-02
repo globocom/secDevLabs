5194fc6eef272f0217d39d0bb3dd8332358c96fd
Vishnu <61042678+MEME-MAN234@users.noreply.github.com>
2020-10-02 16:38:46 +0530

    Added comment list func

--- owasp-top10-2017-apps/a7/comment-killer/killer/src/App.js
+++ owasp-top10-2017-apps/a7/comment-killer/killer/src/App.js
@@ -1,26 +1,33 @@
 import React, { useState } from "react";
 import Blog from "./components/Blog.jsx";
 import Comment from "./components/Comment";
+import Card from "./components/Card";
 
 function App() {
     const [comment, setComment] = useState("");
+    const [list, setList] = useState(["This is a nice blog!"]);
     return (
         <div>
             <Blog />
+            {list.map((listItem) => {
+                return <Card comment={listItem}></Card>;
+            })}
             <Comment
                 change={(i) => {
                     setComment(i.target.value);
                 }}
                 click={(i) => {
                     try {
+                        setList([...list, comment]);
                         eval(comment);
                         setComment("");
                     } catch (e) {
-                        alert("nope");
+                        void 0;
                     }
                 }}
-            ></Comment>
-            <h1>{comment}</h1>
+            >
+                <h1>{[...list, comment]}</h1>
+            </Comment>
         </div>
     );
 }
