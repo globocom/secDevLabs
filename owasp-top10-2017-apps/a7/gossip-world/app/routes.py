ae4c80e83adda48cc27e5dce8e6e56715aa1e8ad
thiagolotufo <56851969+thiagolotufo@users.noreply.github.com>
2021-04-13 14:05:53 -0300

    Update routes.py

--- owasp-top10-2017-apps/a7/gossip-world/app/routes.py
+++ owasp-top10-2017-apps/a7/gossip-world/app/routes.py
@@ -15,7 +15,8 @@ from flask import (
     redirect,
     flash,
     make_response,
-    session
+    session,
+    escape
 )
 from flask_bootstrap import Bootstrap
 from model.password import Password
@@ -78,7 +79,7 @@ def root():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('username')
+        username = escape(request.form.get('username'))
         psw = Password(request.form.get('password').encode('utf-8'))
         user_password, success = database.get_user_password(username)
 
@@ -139,7 +140,7 @@ def newuser():
 @app.route('/gossip', methods=['GET'])
 @login_required
 def all_gossips():
-    search = request.args.get('search')
+    search = escape(request.args.get('search'))
     search_flag = 0
     if search is not None:
         gossips, success = database.search_gossips(search)
@@ -163,7 +164,7 @@ def all_gossips():
 @login_required
 def gossip(id):
     if request.method == 'POST':
-        comment = request.form.get('comment')
+        comment = escape(request.form.get('comment'))
         user = session.get('username')
         date = datetime.datetime.now()
         if comment == '':
@@ -198,9 +199,9 @@ def gossip(id):
 @login_required
 def newgossip():
     if request.method == 'POST':
-        text = request.form.get('text')
-        subtitle = request.form.get('subtitle')
-        title = request.form.get('title')
+        text = escape(request.form.get('text'))
+        subtitle = escape(request.form.get('subtitle'))
+        title = escape(request.form.get('title'))
         author = session.get('username')
         date = datetime.datetime.now()
         if author is None or text is None or subtitle is None or title is None:
