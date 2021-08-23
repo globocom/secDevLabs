eb7fb3e63420860ef9a90e44c5446ac49a042666
João Pacheco <joao.pacheco@g.globo>
2021-08-23 11:57:43 -0300

    [A1 - SSType]

--- owasp-top10-2017-apps/a1/sstype/src/server.py
+++ owasp-top10-2017-apps/a1/sstype/src/server.py
@@ -3,18 +3,11 @@ import tornado.ioloop
 import tornado.web
 import os
 
-TEMPLATE = open(os.path.join(os.path.dirname(__file__)) + "/public/index.html", 'r').readlines()
-
-tmpl = ''
-for t in TEMPLATE:
-    	tmpl += t
-
 class MainHandler(tornado.web.RequestHandler):
 
     def get(self):
         name = self.get_argument('name', '')
-        template_data = tmpl.replace("NAMEHERE",name)
-        t = tornado.template.Template(template_data)
+        t = tornado.template.Template(open(os.path.join(os.path.dirname(__file__)) + "/public/index.html", 'r').read())
         self.write(t.generate(name=name))
 
 application = tornado.web.Application([
