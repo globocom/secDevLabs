ec9d9d3944169c6c17c8cd4452cd8896a2b49355
João Pacheco <joao.pacheco@g.globo>
2021-08-24 11:07:55 -0300

    [A2 - Insecure go project]

--- owasp-top10-2017-apps/a2/insecure-go-project/deployments/mongo-init.sh
+++ owasp-top10-2017-apps/a2/insecure-go-project/deployments/mongo-init.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
+mongo << EOF
 var db = connect("mongodb://localhost/insecure_go_project");
 
 db.createUser(
     {
-        user: "u_insecure_go_project",
-        pwd: "svGX8SViufvYYNu6m3Kv",
+        user: "$MONGO_USER",
+        pwd: "$MONGO_PASS",
         roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
     }
 );
+EOF
