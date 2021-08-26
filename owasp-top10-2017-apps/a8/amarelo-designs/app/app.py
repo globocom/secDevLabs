fe586c7132f65a7f8d1cdd652428d1c8011327e2
João Pacheco <joao.pacheco@g.globo>
2021-08-26 16:10:45 -0300

    [A8 - Amarelo Designs]

--- owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
+++ owasp-top10-2017-apps/a8/amarelo-designs/app/app.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 
-from flask import Flask, request, make_response, render_template, redirect, flash
+from flask import Flask, session, request, make_response, render_template, redirect, flash
 import uuid
-import pickle
+import os
 import base64
 app = Flask(__name__)
-
+app.secret_key = os.environ["SECRET_KEY"]
 
 @app.route("/")
 def ola():
@@ -17,14 +17,10 @@ def login():
         username = request.values.get('username')
         password = request.values.get('password')
     
-        if username == "admin" and password == "admin":
-            token = str(uuid.uuid4().hex)
-            cookie = { "username":username, "admin":True, "sessionId":token }
-            pickle_resultado = pickle.dumps(cookie)
-            encodedSessionCookie = base64.b64encode(pickle_resultado)
-            resp = make_response(redirect("/user"))
-            resp.set_cookie("sessionId", encodedSessionCookie)
-            return resp
+        if username == os.environ["ADMIN_CRED_USER"] and password == os.environ["ADMIN_CRED_PASS"]:
+            session['username'] = username
+            session['admin'] = True
+            return make_response(redirect("/user"))
 
         else:
             return redirect("/admin")
@@ -35,13 +31,9 @@ def login():
 @app.route("/user", methods=['GET'])
 def userInfo():
     cookie = request.cookies.get("sessionId")
-    if cookie == None:
+    if 'username' not in session:
         return "Não Autorizado!"
-    cookie = pickle.loads(base64.b64decode(cookie))
-
     return render_template('user.html')
-    
-
 
 
 if __name__ == '__main__':
