23055db0f1f2a8e945209e13f80d495077d524cc
Henrique de Morais Porto <henrique.morais@g.globo>
2022-03-23 12:02:46 -0300

    correção PR

--- owasp-top10-2021-apps/a1/ecommerce-api/app/handlers/handlers.go
+++ owasp-top10-2021-apps/a1/ecommerce-api/app/handlers/handlers.go
@@ -21,15 +21,15 @@ func GetTicket(c echo.Context) error {
 	id := c.Param("id")
 	cookie, err := c.Cookie("sessionIDa5")
 	if err != nil {
-        return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Cookie Error."})
-    }
+		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Cookie Error."})
+	}
 	userDataQuery := map[string]interface{}{"userID": id}
 	userDataResult, err := db.GetUserData(userDataQuery)
 	if err != nil {
 		// could not find this user in MongoDB (or MongoDB err connection)
 		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error finding this UserID."})
 	}
-	
+
 	claims := &types.Claims{}
 	tkn, err := jwt.ParseWithClaims(cookie.Value, claims, func(token *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("JWT_KEY")), nil
@@ -44,18 +44,19 @@ func GetTicket(c echo.Context) error {
 		return c.JSON(http.StatusUnauthorized, map[string]string{"result": "error", "details": "Error StatusUnauthorized."})
 	}
 
-	if claims.Username == userDataResult.Username{
-		format := c.QueryParam("format")
-		if format == "json" {
-			return c.JSON(http.StatusOK, map[string]string{
-				"result":   "success",
-				"username": userDataResult.Username,
-				"ticket":   userDataResult.Ticket,
-			})
-		}
-
-		msgTicket := fmt.Sprintf("Hey, %s! This is your ticket: %s\n", userDataResult.Username, userDataResult.Ticket)
-		return c.String(http.StatusOK, msgTicket)
+	if claims.Username != userDataResult.Username {
+		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Access denied."})
 	}
-	return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Access denied."})
+
+	format := c.QueryParam("format")
+	if format == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"result":   "success",
+			"username": userDataResult.Username,
+			"ticket":   userDataResult.Ticket,
+		})
+	}
+
+	msgTicket := fmt.Sprintf("Hey, %s! This is your ticket: %s\n", userDataResult.Username, userDataResult.Ticket)
+	return c.String(http.StatusOK, msgTicket)
 }
