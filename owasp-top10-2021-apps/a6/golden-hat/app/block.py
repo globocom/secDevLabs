4415e98c53a9b6d11f7bd1d73d87dd936b284af0
Henrique de Morais Porto <henriqueporto@id.uff.br>
2022-04-06 15:27:53 -0300

    Solution HTTP Request Smuggling

--- owasp-top10-2021-apps/a6/golden-hat/app/block.py
+++ owasp-top10-2021-apps/a6/golden-hat/app/block.py
@@ -4,4 +4,3 @@ import re
 def request(flow):
     if 'golden.secret' in flow.request.url or re.match(r'^http://127.0.0.1:8000/[a-z._/]*$', flow.request.url) is None:
         flow.response = http.HTTPResponse.make(401, b"Haha! Only allowed hats can see this page!")
-    flow.response.headers["Via"] = "mitmproxy/5.3.0"
