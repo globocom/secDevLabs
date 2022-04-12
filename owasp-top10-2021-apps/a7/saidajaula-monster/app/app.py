f2cffdf93c7a704ba7f07077439cd5f208debf33
Henrique de Morais Porto <henriqueporto@id.uff.br>
2022-04-11 23:38:49 -0300

    Solução 01

--- owasp-top10-2021-apps/a7/saidajaula-monster/app/app.py
+++ owasp-top10-2021-apps/a7/saidajaula-monster/app/app.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask, request, make_response, render_template, redirect, Markup
+from flask import Flask, request, render_template, redirect, session
 from model.password import Password
 from model.db import DataBase
-import base64
 import os
-import json
-import hashlib
 import uuid
 from functools import wraps
-
+from datetime import timedelta
 
 app = Flask(__name__)
 database = DataBase(os.environ.get('A2_DATABASE_HOST'),
@@ -17,44 +14,30 @@ database = DataBase(os.environ.get('A2_DATABASE_HOST'),
                     os.environ.get('A2_DATABASE_PASSWORD'),
                     os.environ.get('A2_DATABASE_NAME'))
 
+app.config['SECRET_KEY'] = os.environ.get("SECRET")
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 
 def login_admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        cookie = request.cookies.get("sessionId", "")
-        cookie = base64.b64decode(cookie).decode("utf-8")
-        cookie_separado = cookie.split('.')
-        if(len(cookie_separado) != 2):
-            return "Invalid cookie!"
-        hash_cookie = hashlib.sha256(cookie_separado[0].encode('utf-8')).hexdigest()
-        if (hash_cookie != cookie_separado[1]):
+        if not all(k in session for k in ('username','permissao')):
             return redirect("/login")
-        j = json.loads(cookie_separado[0])
-        if j.get("permissao") != 1:
+        if session['permissao'] != 1:
             return "You don't have permission to access this route. You are not an admin. \n"
         return f(*args, **kwargs)
     return decorated_function
 
-
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        cookie = request.cookies.get("sessionId", "")
-        cookie = base64.b64decode(cookie).decode("utf-8")
-        cookie_separado = cookie.split('.')
-        if(len(cookie_separado) != 2):
-            return "Invalid cookie! \n"
-        hash_cookie = hashlib.sha256(cookie_separado[0].encode('utf-8')).hexdigest()
-        if (hash_cookie != cookie_separado[1]):
+        if not all(k in session for k in ('username','permissao')):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
 
-
 @app.route("/", methods=['GET'])
 def home():
     return render_template('index.html')
-
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -70,15 +53,17 @@ def register():
             return "Error! You have to pass username and password! \n"
         elif form_password != form_password2:
             return "Error! Passwords must be the same! \n"
-
-        guid = str(uuid.uuid4())
-        password = Password(form_password, form_username, guid)
-        hashed_password = password.get_hashed_password()
-        message, success = database.insert_user(guid, form_username, hashed_password)
-        if success:
-            return render_template('login.html')
-        return "Error: account creation failed \n"
-
+        
+        result, success = database.get_user(form_username)
+        if success and result == None:
+            guid = str(uuid.uuid4())
+            password = Password(form_password, form_username, guid)
+            hashed_password = password.get_hashed_password()
+            message, success = database.insert_user(guid, form_username, hashed_password)
+            if success:
+                return render_template('login.html')
+            return "Error: account creation failed; Database_message: {}\n".format(message)
+        return "User already exist."
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -92,37 +77,27 @@ def login():
             return "Error! You have to pass username and password! \n"
 
         result, success = database.get_user(form_username)
-        if not success:
-            return "Login failed! \n"
-
-        if result is None:
+        #result[0]=password_hash; result[1]=permissao; result[2]=salt;
+        if (not success) or (result is None):
             return "Login failed! \n"
 
         password = Password(form_password, form_username, result[2])
         if not password.validate_password(result[0]):
             return "Login failed! \n"
 
-        cookie_dic = {"permissao": result[1], "username": form_username}
-        cookie = json.dumps(cookie_dic)
-        hash_cookie = hashlib.sha256(cookie.encode('utf-8')).hexdigest()
-        cookie_done = '.'.join([cookie,hash_cookie])
-        cookie_done = base64.b64encode(str(cookie_done).encode("utf-8"))
-        resp = make_response("Logged in!")
-        resp.set_cookie("sessionId", cookie_done)
-        return resp
-
+        session['permissao'] = result[1]
+        session['username'] = form_username
+        return "Logged in!"
 
 @app.route("/admin", methods=['GET'])
 @login_admin_required
 def admin():
     return "You are an admin! \n"
 
-
 @app.route("/user", methods=['GET'])
 @login_required
 def userInfo():
     return "You are an user! \n"
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=10002)
