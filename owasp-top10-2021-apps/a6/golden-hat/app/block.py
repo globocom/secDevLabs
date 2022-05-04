7c9f7b2b894f33ba7262323b5a29bfd7b3dc5046
Henrique de Morais Porto <henriqueporto@id.uff.br>
2022-05-04 13:03:43 -0300

    Correção solução

--- owasp-top10-2021-apps/a6/golden-hat/app/block.py
+++ owasp-top10-2021-apps/a6/golden-hat/app/block.py
@@ -3,4 +3,4 @@ import re
 
 def request(flow):
     if 'golden.secret' in flow.request.url or re.match(r'^http://127.0.0.1:8000/[a-z._/]*$', flow.request.url) is None:
-        flow.response = http.HTTPResponse.make(401, b"Haha! Only allowed hats can see this page!")
+        flow.response = http.Response.make(401, b"Haha! Only allowed hats can see this page!")
