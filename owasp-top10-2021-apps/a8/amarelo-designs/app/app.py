f0687a7abb021c2fbed94dd88eea2065c4cdefd3
ines <ines.cardinot@g.globo>
2022-05-30 16:26:25 -0300

    yellow

--- owasp-top10-2021-apps/a8/amarelo-designs/app/app.py
+++ owasp-top10-2021-apps/a8/amarelo-designs/app/app.py
@@ -1,11 +1,17 @@
 # coding: utf-8
 
+from distutils.log import error
+from secrets import token_urlsafe
 from flask import Flask, request, make_response, render_template, redirect, flash
 import uuid
-import pickle
-import base64
+import os
+import jwt
+from datetime import datetime, timedelta
 app = Flask(__name__)
 
+admin_user = os.environ.get("ADMIN_USER")
+admin_pass = os.environ.get("ADMIN_PASS")
+jwt_secret_key = os.environ.get("JWT_SECRET_KEY")
 
 @app.route("/")
 def ola():
@@ -13,34 +19,54 @@ def ola():
 
 @app.route("/admin", methods=['GET','POST'])
 def login():
+
+    
+
     if request.method == 'POST':
         username = request.values.get('username')
         password = request.values.get('password')
     
-        if username == "admin" and password == "admin":
-            token = str(uuid.uuid4().hex)
-            cookie = { "username":username, "admin":True, "sessionId":token }
-            pickle_resultado = pickle.dumps(cookie)
-            encodedSessionCookie = base64.b64encode(pickle_resultado)
+        if username == admin_user and password == admin_pass:
+            session_id = str(uuid.uuid4().hex)
+
+            claims = {
+                "username": username,
+                "admin": True,
+                "sessionId": session_id,
+                "exp": datetime.utcnow() + timedelta(seconds = 30)
+
+            }
+            try:
+                token = jwt.encode(claims, jwt_secret_key, algorithms = "HS256")
+            except:
+                return "Error!\n"
             resp = make_response(redirect("/user"))
-            resp.set_cookie("sessionId", encodedSessionCookie)
+            resp.set_cookie("sessionId", token)
             return resp
 
         else:
             return redirect("/admin")
 
     else:
-        return render_template('admin.html')
+        return render_template('user.html')
 
 @app.route("/user", methods=['GET'])
 def userInfo():
-    cookie = request.cookies.get("sessionId")
-    if cookie == None:
-        return "Não Autorizado!"
-    cookie = pickle.loads(base64.b64decode(cookie))
+   
+    token = request.cookies.get("sessionId", "")
 
-    return render_template('user.html')
-    
+       
+    if token["admin"] == None:
+            return "Não Autorizado!"
+            
+    try:
+
+        decoded_token = jwt.decode(token, jwt_secret_key, algorithms = "HS256")
+
+    except:
+        return render_template('/admin')
+                
+    return redirect("user.html")
 
 
 
