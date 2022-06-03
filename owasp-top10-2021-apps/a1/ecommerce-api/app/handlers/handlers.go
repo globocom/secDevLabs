59842f255e99878378d76ee53eaffe3eb2dc899c
vitor-mauricio <vitormauriciodf3@gmail.com>
2022-06-03 10:41:28 -0300

    Fixing ecommerce-api

--- owasp-top10-2021-apps/a1/ecommerce-api/app/handlers/handlers.go
+++ owasp-top10-2021-apps/a1/ecommerce-api/app/handlers/handlers.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"net/http"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/globocom/secDevLabs/owasp-top10-2021-apps/a1/ecommerce-api/app/db"
 	"github.com/labstack/echo"
 )
+
+type Claims struct {
+	Username string `json:"name"`
+	jwt.StandardClaims
+}
 
 // HealthCheck is the heath check function.
 func HealthCheck(c echo.Context) error {
@@ -16,8 +22,29 @@ func HealthCheck(c echo.Context) error {
 // GetTicket returns the userID ticket.
 func GetTicket(c echo.Context) error {
 	id := c.Param("id")
+	cookie, err := c.Cookie("sessionIDa5")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error to get cookie.\n"})
+	}
+	encodedToken := cookie.Value
+	token, _, err := new(jwt.Parser).ParseUnverified(encodedToken, &Claims{})
+
+	if err != nil {
+		return err
+	}
+
 	userDataQuery := map[string]interface{}{"userID": id}
 	userDataResult, err := db.GetUserData(userDataQuery)
+
+	if claims, ok := token.Claims.(*Claims); ok {
+		if claims.Username != userDataResult.Username {
+			return c.JSON(http.StatusForbidden, map[string]string{"result": "forbidden", "details": "Token valitation error\n"})
+		}
+		fmt.Println(claims.Username)
+	} else {
+		fmt.Println(err)
+	}
+
 	if err != nil {
 		// could not find this user in MongoDB (or MongoDB err connection)
 		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error finding this UserID."})
