4465e347e26ea5f6925a9d5519588f699cab439f
Ray <raishatinoco@gmail.com>
2024-04-16 19:02:10 -0300

    Adicionando medida de segurança

--- owasp-top10-2021-apps/a5/vinijr-blog/app/contact.php
+++ owasp-top10-2021-apps/a5/vinijr-blog/app/contact.php
@@ -2,11 +2,20 @@
 $xmlfile = file_get_contents('php://input');
 $dom = new DOMDocument();
 $dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD);
-$contact = simplexml_import_dom($dom);
-$name = $contact->name;
-$email = $contact->email;
-$subject = $contact->subject;
-$message = $contact->message;
+// Removendo a resolução de entidades externas 
 
-echo "Thanks for the message, $name !";
+$dom->resolveExternals = false;
+$dom->substituteEntities = false;
+// Verificando se o XML é válido antes de prosseguir
+if ($dom->validate()) {
+    $contact = simplexml_import_dom($dom);
+    $name = $contact->name;
+    $email = $contact->email;
+    $subject = $contact->subject;
+    $message = $contact->message;
+
+    echo "Thanks for the message, $name !";
+} else {
+    echo "Invalid XML input!";
+}
 ?>
