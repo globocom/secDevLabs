140755a9ae05e915c4a7a8cb6a7c75d0244b1888
William Franco <william.franco@g.globo>
2024-08-07 16:11:41 -0300

    feat: fixing stegonography

--- owasp-top10-2021-apps/a5/stegonography/deployments/generate-env.sh
+++ owasp-top10-2021-apps/a5/stegonography/deployments/generate-env.sh
@@ -4,23 +4,41 @@
 #
 
 # API environment variables
-SECRET=$RANDOM$RANDOM
+SECRET=$(pwgen -s $KEY_LENGTH 1 | md5sum | awk '{print $1}')
+USER=$(pwgen -s $KEY_LENGTH 1 | md5sum | awk '{print $1}')
+PASS=$(pwgen -s $KEY_LENGTH 1 | md5sum | awk '{print $1}')
 
 echo "#.env" > app/.env
 echo "SECRET=$SECRET" >> app/.env
+# echo "USER=$USER" >> app/.env
+# echo "PASS=$PASS" >> app/.env
 
 
 # Database environment variables
-# MONGO_DATABASE="stego"
-MONGO_DATABASE_USERNAME=User$RANDOM$RANDOM
-MONGO_DATABASE_PASSWORD=Pass$RANDOM$RANDOM
-# MONGO_PORT=27017
+MONGO_DATABASE="stego$(pwgen -s $KEY_LENGTH 1 | md5sum | awk '{print $1}')"
+MONGO_DATABASE_USERNAME=User$(pwgen -s $KEY_LENGTH 1 | md5sum | awk '{print $1}')
+MONGO_DATABASE_PASSWORD=Pass$(pwgen -s $KEY_LENGTH 1 | md5sum | awk '{print $1}')
+MONGO_PORT=27017
+MONGO_ROOT_PASSWORD=Root$(pwgen -s $KEY_LENGTH 1 | md5sum | awk '{print $1}')
 
 echo "#" > deployments/.dockers.env
 echo "# This file is auto generated and contains all environment variables needed by Stegonography's database" >> deployments/.dockers.env
 echo "#" >> deployments/.dockers.env
 echo "MONGO_ROOT_PASSWORD=$MONGO_ROOT_PASSWORD" >> deployments/.dockers.env
 echo "MONGO_DATABASE=$MONGO_DATABASE" >> deployments/.dockers.env
-echo "MONGO_USER=$MONGO_USER" >> deployments/.dockers.env
-echo "MONGO_PASSWORD=$MONGO_PASSWORD" >> deployments/.dockers.env
+echo "MONGO_USER=$MONGO_DATABASE_USERNAME" >> deployments/.dockers.env
+echo "MONGO_PASSWORD=$MONGO_DATABASE_PASSWORD" >> deployments/.dockers.env
 echo "MONGO_PORT=$MONGO_PORT" >> deployments/.dockers.env
+echo "USER=$USER" >> deployments/.dockers.env
+echo "PASS=$PASS" >> deployments/.dockers.env
+
+
+
+
+# KEY_LENGTH=32
+
+# # Gera uma chave criptograficamente segura
+# SECURE_KEY=$(pwgen -s $KEY_LENGTH 1 | md5sum | awk '{print $1}')
+
+# # Exibe a chave gerada
+# echo "Chave gerada: $SECURE_KEY"
