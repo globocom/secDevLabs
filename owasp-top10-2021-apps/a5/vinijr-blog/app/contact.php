7301abade7d0b7066b717def4f064256c6420935
José Lira <joserobertolira@gmail.com>
2024-10-07 16:59:07 -0300

    fix: "Mitigated XXE vulnerability in contact.php"

--- owasp-top10-2021-apps/a5/vinijr-blog/app/contact.php
+++ owasp-top10-2021-apps/a5/vinijr-blog/app/contact.php
@@ -1,12 +1,26 @@
 <?php
-$xmlfile = file_get_contents('php://input');
-$dom = new DOMDocument();
-$dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD);
-$contact = simplexml_import_dom($dom);
-$name = $contact->name;
-$email = $contact->email;
-$subject = $contact->subject;
-$message = $contact->message;
+libxml_disable_entity_loader(true);
 
-echo "Thanks for the message, $name !";
-?>
+$xmlfile = file_get_contents('php://input');
+
+$dom = new DOMDocument();
+
+$dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD | LIBXML_NOERROR | LIBXML_NOWARNING);
+
+$contact = simplexml_import_dom($dom);
+
+if (isset($contact->name) && isset($contact->email) && isset($contact->subject) && isset($contact->message)) {
+    $name    = htmlspecialchars($contact->name, ENT_QUOTES, 'UTF-8');
+    $email   = filter_var($contact->email, FILTER_VALIDATE_EMAIL);
+    $subject = htmlspecialchars($contact->subject, ENT_QUOTES, 'UTF-8');
+    $message = htmlspecialchars($contact->message, ENT_QUOTES, 'UTF-8');
+
+    if ($email !== false) {
+        echo "Thanks for the message, $name!";
+    } else {
+        echo "Invalid email address!";
+    }
+} else {
+    echo "Invalid XML format!";
+}
+
